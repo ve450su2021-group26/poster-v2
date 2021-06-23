89c394e0173c63900865c2140059fef4f8cae89b
liqin-personal <graves_zhang@sjtu.edu.cn>
2021-06-24 00:59:14 +0800

    testing - bootstrap navbar added

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'font-awesome/css/font-awesome.min.css'
+import { BootstrapVue,IconsPlugin} from 'bootstrap-vue'
+import 'bootstrap'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
